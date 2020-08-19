1828cf96576cd399bd73f8e672e9ba5020bfc116
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2020-08-20 00:26:20 +0530

    [CAN] Regenerated demo for can_can_normal_operation_blocking_pic32mx_eth_sk2
    MH3-39106
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/can/can_normal_operation_blocking/firmware/src/config/pic32mx_eth_sk2/peripheral/gpio/plib_gpio.h
+++ apps/can/can_normal_operation_blocking/firmware/src/config/pic32mx_eth_sk2/peripheral/gpio/plib_gpio.h
@@ -61,21 +61,23 @@
 // *****************************************************************************
 // *****************************************************************************
 
+
 /*** Macros for LED1 pin ***/
 #define LED1_Set()               (LATDSET = (1<<0))
 #define LED1_Clear()             (LATDCLR = (1<<0))
 #define LED1_Toggle()            (LATDINV= (1<<0))
-#define LED1_Get()               ((PORTD >> 0) & 0x1)
 #define LED1_OutputEnable()      (TRISDCLR = (1<<0))
 #define LED1_InputEnable()       (TRISDSET = (1<<0))
+#define LED1_Get()               ((PORTD >> 0) & 0x1)
 #define LED1_PIN                  GPIO_PIN_RD0
+
 /*** Macros for SW1 pin ***/
 #define SW1_Set()               (LATDSET = (1<<6))
 #define SW1_Clear()             (LATDCLR = (1<<6))
 #define SW1_Toggle()            (LATDINV= (1<<6))
-#define SW1_Get()               ((PORTD >> 6) & 0x1)
 #define SW1_OutputEnable()      (TRISDCLR = (1<<6))
 #define SW1_InputEnable()       (TRISDSET = (1<<6))
+#define SW1_Get()               ((PORTD >> 6) & 0x1)
 #define SW1_PIN                  GPIO_PIN_RD6
 
 
@@ -275,42 +277,42 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-    GPIO_PortWrite(pin>>4, (uint32_t)(0x1) << (pin & 0xF), (uint32_t)(value) << (pin & 0xF));
+    GPIO_PortWrite((GPIO_PORT)(pin>>4), (uint32_t)(0x1) << (pin & 0xF), (uint32_t)(value) << (pin & 0xF));
 }
 
 static inline bool GPIO_PinRead(GPIO_PIN pin)
 {
-    return (bool)(((GPIO_PortRead(pin>>4)) >> (pin & 0xF)) & 0x1);
+    return (bool)(((GPIO_PortRead((GPIO_PORT)(pin>>4))) >> (pin & 0xF)) & 0x1);
 }
 
 static inline bool GPIO_PinLatchRead(GPIO_PIN pin)
 {
-    return (bool)((GPIO_PortLatchRead(pin>>4) >> (pin & 0xF)) & 0x1);
+    return (bool)((GPIO_PortLatchRead((GPIO_PORT)(pin>>4)) >> (pin & 0xF)) & 0x1);
 }
 
 static inline void GPIO_PinToggle(GPIO_PIN pin)
 {
-    GPIO_PortToggle(pin>>4, 0x1 << (pin & 0xF));
+    GPIO_PortToggle((GPIO_PORT)(pin>>4), 0x1 << (pin & 0xF));
 }
 
 static inline void GPIO_PinSet(GPIO_PIN pin)
 {
-    GPIO_PortSet(pin>>4, 0x1 << (pin & 0xF));
+    GPIO_PortSet((GPIO_PORT)(pin>>4), 0x1 << (pin & 0xF));
 }
 
 static inline void GPIO_PinClear(GPIO_PIN pin)
 {
-    GPIO_PortClear(pin>>4, 0x1 << (pin & 0xF));
+    GPIO_PortClear((GPIO_PORT)(pin>>4), 0x1 << (pin & 0xF));
 }
 
 static inline void GPIO_PinInputEnable(GPIO_PIN pin)
 {
-    GPIO_PortInputEnable(pin>>4, 0x1 << (pin & 0xF));
+    GPIO_PortInputEnable((GPIO_PORT)(pin>>4), 0x1 << (pin & 0xF));
 }
 
 static inline void GPIO_PinOutputEnable(GPIO_PIN pin)
 {
-    GPIO_PortOutputEnable(pin>>4, 0x1 << (pin & 0xF));
+    GPIO_PortOutputEnable((GPIO_PORT)(pin>>4), 0x1 << (pin & 0xF));
 }
 
 
