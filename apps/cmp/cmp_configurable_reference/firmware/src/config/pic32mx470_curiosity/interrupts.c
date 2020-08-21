8325f670ec1170e6ac3eaf15850de181d5d56643
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-08-21 17:23:16 +0530

    [CMP] Regenerated demo for cmp_cmp_configurable_reference_pic32mx470_curiosity
    MH3-39116
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/cmp/cmp_configurable_reference/firmware/src/config/pic32mx470_curiosity/interrupts.c
+++ apps/cmp/cmp_configurable_reference/firmware/src/config/pic32mx470_curiosity/interrupts.c
@@ -63,7 +63,7 @@ void COMPARATOR_1_InterruptHandler( void );
 
 
 /* All the handlers are defined here.  Each will call its PLIB-specific function. */
-void __ISR(_COMPARATOR_1_VECTOR, ipl1AUTO) COMPARATOR_1_Handler (void)
+void __ISR(_COMPARATOR_1_VECTOR, ipl1SOFT) COMPARATOR_1_Handler (void)
 {
     COMPARATOR_1_InterruptHandler();
 }
