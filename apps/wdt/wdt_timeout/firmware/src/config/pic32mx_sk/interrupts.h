a34ff325e0d673d60ebe4362061bb64f060c6d21
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-01-28 16:28:54 +0530

    [WDT] Regenerated demo for wdt_wdt_timeout_pic32mx_sk
    MH3-48416
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/wdt/wdt_timeout/firmware/src/config/pic32mx_sk/interrupts.h
+++ apps/wdt/wdt_timeout/firmware/src/config/pic32mx_sk/interrupts.h
@@ -1,32 +1,22 @@
 /*******************************************************************************
-  SYS CLK Static Functions for Clock System Service
+ System Interrupts File
 
   Company:
     Microchip Technology Inc.
 
   File Name:
-    plib_clk.c
+    interrupt.h
 
   Summary:
-    SYS CLK static function implementations for the Clock System Service.
+    Interrupt vectors mapping
 
   Description:
-    The Clock System Service provides a simple interface to manage the
-    oscillators on Microchip microcontrollers. This file defines the static
-    implementation for the Clock System Service.
+    This file contains declarations of device vectors used by Harmony 3
+ *******************************************************************************/
 
-  Remarks:
-    Static functions incorporate all system clock configuration settings as
-    determined by the user via the Microchip Harmony Configurator GUI.
-    It provides static version of the routines, eliminating the need for an
-    object ID or object handle.
-
-    Static single-open interfaces also eliminate the need for the open handle.
-
-*******************************************************************************/
-
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -46,49 +36,26 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*******************************************************************************/
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+#ifndef INTERRUPTS_H
+#define INTERRUPTS_H
 
 // *****************************************************************************
 // *****************************************************************************
-// Section: Include Files
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include <stdint.h>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Handler Routines
 // *****************************************************************************
 // *****************************************************************************
 
-#include "device.h"
-#include "plib_clk.h"
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: File Scope Functions
-// *****************************************************************************
-// *****************************************************************************
 
-// *****************************************************************************
-/* Function:
-    void CLK_Initialize( void )
-
-  Summary:
-    Initializes hardware and internal data structure of the System Clock.
-
-  Description:
-    This function initializes the hardware and internal data structure of System
-    Clock Service.
-
-  Remarks:
-    This is configuration values for the static version of the Clock System
-    Service module is determined by the user via the MHC GUI.
-
-    The objective is to eliminate the user's need to be knowledgeable in the
-    function of the 'configuration bits' to configure the system oscillators.
-*/
-
-void CLK_Initialize( void )
-{
-
-    /* Code for fuse settings can be found in "initialization.c" */
-    
-
-    /* Wait for PLL to be locked */
-    while(!OSCCONbits.LOCK);
-
-}
+#endif // INTERRUPTS_H
