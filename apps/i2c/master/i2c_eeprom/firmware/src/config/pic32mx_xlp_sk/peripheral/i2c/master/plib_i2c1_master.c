a238cdb5de2c74ea68ca38085398dd0ef187c9dc
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-03 15:43:22 +0530

    [I2C] Regenerated demo for i2c_master_i2c_eeprom_pic32mx_xlp_sk
    MH3-57408
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/peripheral/i2c/master/plib_i2c1_master.c
+++ apps/i2c/master/i2c_eeprom/firmware/src/config/pic32mx_xlp_sk/peripheral/i2c/master/plib_i2c1_master.c
@@ -68,7 +68,7 @@ void I2C1_Initialize(void)
     /* Disable the I2C Bus collision interrupt */
     IEC1CLR = _IEC1_I2C1BIE_MASK;
 
-    I2C1BRG = 357;
+    I2C1BRG = 356;
 
     I2C1CONCLR = _I2C1CON_SIDL_MASK;
     I2C1CONCLR = _I2C1CON_DISSLW_MASK;
