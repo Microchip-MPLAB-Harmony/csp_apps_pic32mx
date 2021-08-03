86d3d6a547b10a6f92ef51c67f16a63c662be919
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-03 15:47:45 +0530

    [POWER] Regenerated demo for power_low_power_modes_pic32mx_xlp_sk
    MH3-57420
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/power/low_power_modes/firmware/src/config/pic32mx_xlp_sk/peripheral/gpio/plib_gpio.c
+++ apps/power/low_power_modes/firmware/src/config/pic32mx_xlp_sk/peripheral/gpio/plib_gpio.c
@@ -77,11 +77,6 @@ void GPIO_Initialize ( void )
     TRISCCLR = 0x20; /* Direction Control */
 
 
-    /* unlock system for PPS configuration */
-    SYSKEY = 0x00000000;
-    SYSKEY = 0xAA996655;
-    SYSKEY = 0x556699AA;
-    CFGCONbits.IOLOCK = 0;
 
     /* PPS Input Remapping */
     U1RXR = 5;
@@ -89,9 +84,6 @@ void GPIO_Initialize ( void )
     /* PPS Output Remapping */
     RPC7R = 1;
 
-    /* Lock back the system after PPS configuration */
-    CFGCONbits.IOLOCK = 1;
-    SYSKEY = 0x00000000; 
 
     uint32_t i;
     /* Initialize Interrupt Pin data structures */
