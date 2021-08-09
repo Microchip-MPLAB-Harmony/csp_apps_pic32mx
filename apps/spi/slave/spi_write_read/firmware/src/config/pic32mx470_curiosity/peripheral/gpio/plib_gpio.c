21f8488cefd315bfdc8d5401967cf5ebb678de61
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-09 23:28:53 +0530

    [SPI] Regenerated demo for spi_slave_spi_write_read_pic32mx470_curiosity
    MH3-57430
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/spi/slave/spi_write_read/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/spi/slave/spi_write_read/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
@@ -83,11 +83,6 @@ void GPIO_Initialize ( void )
     /* PORTG Initialization */
 
 
-    /* unlock system for PPS configuration */
-    SYSKEY = 0x00000000;
-    SYSKEY = 0xAA996655;
-    SYSKEY = 0x556699AA;
-    CFGCONbits.IOLOCK = 0;
 
     /* PPS Input Remapping */
     SDI1R = 0;
@@ -96,9 +91,6 @@ void GPIO_Initialize ( void )
     /* PPS Output Remapping */
     RPD5R = 8;
 
-    /* Lock back the system after PPS configuration */
-    CFGCONbits.IOLOCK = 1;
-    SYSKEY = 0x00000000; 
 
     uint32_t i;
     /* Initialize Interrupt Pin data structures */
