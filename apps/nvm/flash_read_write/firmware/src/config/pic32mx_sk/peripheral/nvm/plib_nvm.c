70674568f3ec1a29fb9fb9ecc179e6576c044e8a
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2022-05-25 17:59:33 +0530

    [NVM] Regenerated demo for nvm_flash_read_write_pic32mx_sk
    MH3-66535
    
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/nvm/flash_read_write/firmware/src/config/pic32mx_sk/peripheral/nvm/plib_nvm.c
+++ apps/nvm/flash_read_write/firmware/src/config/pic32mx_sk/peripheral/nvm/plib_nvm.c
@@ -118,6 +118,7 @@ void NVM_InterruptHandler( void )
 static void NVM_StartOperationAtAddress( uint32_t address,  NVM_OPERATION_MODE operation )
 {
     volatile uint32_t processorStatus;
+    unsigned long mTime;
 
     processorStatus = __builtin_disable_interrupts();
 
@@ -132,6 +133,13 @@ static void NVM_StartOperationAtAddress( uint32_t address,  NVM_OPERATION_MODE o
 
     // Set WREN to enable writes to the WR bit and to prevent NVMOP modification
     NVMCONSET = _NVMCON_WREN_MASK;
+
+    mTime = _CP0_GET_COUNT();
+    mTime += ((80000000 / 2 / 1000000) * 6);
+    while ((signed long)(mTime - _CP0_GET_COUNT()) > 0)
+    {
+        Nop();
+    }
 
     // Write the unlock key sequence
     NVMKEY = 0x0;
