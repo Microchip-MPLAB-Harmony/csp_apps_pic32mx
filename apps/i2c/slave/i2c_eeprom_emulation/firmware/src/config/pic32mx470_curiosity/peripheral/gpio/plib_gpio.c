374b857dcdc2a0073640a467254b5274087d41f2
Sumanth Naga Venkata Ranga Suraneni <Sumanth.Suraneni@microchip.com>
2023-05-02 19:41:25 +0530

    [I2C] Regenerated demo for i2c_slave_i2c_eeprom_emulation_pic32mx470_curiosity
    MH3-78130

--- apps/i2c/slave/i2c_eeprom_emulation/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
+++ apps/i2c/slave/i2c_eeprom_emulation/firmware/src/config/pic32mx470_curiosity/peripheral/gpio/plib_gpio.c
@@ -42,6 +42,7 @@
 //DOM-IGNORE-END
 
 #include "plib_gpio.h"
+#include "interrupts.h"
 
 
 
@@ -108,7 +109,7 @@ void GPIO_Initialize ( void )
 */
 uint32_t GPIO_PortRead(GPIO_PORT port)
 {
-    return (*(volatile uint32_t *)(&PORTB + (port * 0x40)));
+    return (*(volatile uint32_t *)(&PORTB + (port * 0x40U)));
 }
 
 // *****************************************************************************
@@ -123,7 +124,7 @@ uint32_t GPIO_PortRead(GPIO_PORT port)
 */
 void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value)
 {
-    *(volatile uint32_t *)(&LATB + (port * 0x40)) = (*(volatile uint32_t *)(&LATB + (port * 0x40)) & (~mask)) | (mask & value);
+    *(volatile uint32_t *)(&LATB + (port * 0x40U)) = (*(volatile uint32_t *)(&LATB + (port * 0x40U)) & (~mask)) | (mask & value);
 }
 
 // *****************************************************************************
@@ -138,7 +139,7 @@ void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value)
 */
 uint32_t GPIO_PortLatchRead(GPIO_PORT port)
 {
-    return (*(volatile uint32_t *)(&LATB + (port * 0x40)));
+    return (*(volatile uint32_t *)(&LATB + (port * 0x40U)));
 }
 
 // *****************************************************************************
@@ -153,7 +154,7 @@ uint32_t GPIO_PortLatchRead(GPIO_PORT port)
 */
 void GPIO_PortSet(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&LATBSET + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&LATBSET + (port * 0x40U)) = mask;
 }
 
 // *****************************************************************************
@@ -168,7 +169,7 @@ void GPIO_PortSet(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortClear(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&LATBCLR + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&LATBCLR + (port * 0x40U)) = mask;
 }
 
 // *****************************************************************************
@@ -183,7 +184,7 @@ void GPIO_PortClear(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortToggle(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&LATBINV + (port * 0x40))= mask;
+    *(volatile uint32_t *)(&LATBINV + (port * 0x40U))= mask;
 }
 
 // *****************************************************************************
@@ -198,7 +199,7 @@ void GPIO_PortToggle(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&TRISBSET + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&TRISBSET + (port * 0x40U)) = mask;
 }
 
 // *****************************************************************************
@@ -213,7 +214,7 @@ void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask)
 */
 void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask)
 {
-    *(volatile uint32_t *)(&TRISBCLR + (port * 0x40)) = mask;
+    *(volatile uint32_t *)(&TRISBCLR + (port * 0x40U)) = mask;
 }
 
 
--- apps/i2c/slave/i2c_eeprom_emulation/firmware/src/config/pic32mx470_curiosity/peripheral/i2c/slave/plib_i2c1_slave.c
+++ apps/i2c/slave/i2c_eeprom_emulation/firmware/src/config/pic32mx470_curiosity/peripheral/i2c/slave/plib_i2c1_slave.c
@@ -49,6 +49,7 @@
 // *****************************************************************************
 #include "device.h"
 #include "plib_i2c1_slave.h"
+#include "interrupts.h"
 
 // *****************************************************************************
 // *****************************************************************************
@@ -58,7 +59,7 @@
 #define I2C1_SLAVE_DATA_SETUP_TIME_CORE_TIMER_CNTS          4
 #define I2C1_SLAVE_RISE_TIME_CORE_TIMER_CNTS                14
 
-static I2C_SLAVE_OBJ i2c1Obj;
+volatile static I2C_SLAVE_OBJ i2c1Obj;
 
 void I2C1_Initialize(void)
 {
@@ -93,7 +94,7 @@ static void I2C1_RiseAndSetupTime(uint8_t sdaState)
 {
     uint32_t startCount, endCount;
 
-    if (sdaState == 0)
+    if (sdaState == 0U)
     {
         endCount = I2C1_SLAVE_DATA_SETUP_TIME_CORE_TIMER_CNTS;
     }
@@ -104,21 +105,25 @@ static void I2C1_RiseAndSetupTime(uint8_t sdaState)
 
     startCount =_CP0_GET_COUNT();
 
-    while((_CP0_GET_COUNT()- startCount) < endCount);
+    while((_CP0_GET_COUNT()- startCount) < endCount)
+    {
+           /* Wait for timeout */
+    }
 }
 
 /* I2C slave state machine */
 static void I2C1_TransferSM(void)
 {
     uint32_t i2c_addr;
-    uint8_t sdaValue = 0;
+    uint8_t sdaValue = 0U;
+    uintptr_t context = i2c1Obj.context;
 
     /* ACK the slave interrupt */
     IFS1CLR = _IFS1_I2C1SIF_MASK;
 
-    if ((I2C1STAT & _I2C1STAT_D_A_MASK) == 0)
+    if ((I2C1STAT & _I2C1STAT_D_A_MASK) == 0U)
     {
-        if (I2C1STAT & _I2C1STAT_RBF_MASK)
+        if ((I2C1STAT & _I2C1STAT_RBF_MASK) != 0U)
         {
             /* Received I2C address must be read out */
             i2c_addr = I2C1RCV;
@@ -127,17 +132,17 @@ static void I2C1_TransferSM(void)
             if (i2c1Obj.callback != NULL)
             {
                 /* Notify that a address match event has occurred */
-                i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_ADDR_MATCH, i2c1Obj.context);
+               (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_ADDR_MATCH, context);
 
-                if (I2C1STAT & _I2C1STAT_R_W_MASK)
+                if ((I2C1STAT & _I2C1STAT_R_W_MASK) != 0U)
                 {
                     /* I2C master wants to read */
-                    if (!(I2C1STAT & _I2C1STAT_TBF_MASK))
+                    if ((I2C1STAT & _I2C1STAT_TBF_MASK) == 0U)
                     {
                         /* In the callback, slave must write to transmit register by calling I2Cx_WriteByte() */
-                        (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, i2c1Obj.context);
+                        (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, context);
 
-                        sdaValue = (i2c1Obj.lastByteWritten & 0x80);
+                        sdaValue = (i2c1Obj.lastByteWritten & 0x80U);
                         I2C1_RiseAndSetupTime(sdaValue);
                     }
                 }
@@ -149,16 +154,16 @@ static void I2C1_TransferSM(void)
     else
     {
         /* Master reads from slave, slave transmits */
-        if (I2C1STAT & _I2C1STAT_R_W_MASK)
+        if ((I2C1STAT & _I2C1STAT_R_W_MASK) != 0U)
         {
-            if ((!(I2C1STAT & _I2C1STAT_TBF_MASK)) && (!(I2C1STAT & _I2C1STAT_ACKSTAT_MASK)))
+            if (((I2C1STAT & (_I2C1STAT_TBF_MASK | _I2C1STAT_ACKSTAT_MASK))  == 0U))
             {
                 if (i2c1Obj.callback != NULL)
                 {
                     /* I2C master wants to read. In the callback, slave must write to transmit register */
-                    (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, i2c1Obj.context);
+                    (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_TX_READY, context);
 
-                    sdaValue = (i2c1Obj.lastByteWritten & 0x80);
+                    sdaValue = (i2c1Obj.lastByteWritten & 0x80U);
                 }
 
                 I2C1_RiseAndSetupTime(sdaValue);
@@ -170,12 +175,12 @@ static void I2C1_TransferSM(void)
         /* Master writes to slave, slave receives */
         else
         {
-            if (I2C1STAT & _I2C1STAT_RBF_MASK)
+            if ((I2C1STAT & _I2C1STAT_RBF_MASK) != 0U)
             {
                 if (i2c1Obj.callback != NULL)
                 {
                     /* I2C master wants to write. In the callback, slave must read data by calling I2Cx_ReadByte()  */
-                    (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_RX_READY, i2c1Obj.context);
+                    (void)i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_RX_READY, context);
                 }
                 /* Data read by the application; release the clock stretch */
                 I2C1CONSET = _I2C1CON_SCLREL_MASK;
@@ -186,35 +191,26 @@ static void I2C1_TransferSM(void)
 
 void I2C1_CallbackRegister(I2C_SLAVE_CALLBACK callback, uintptr_t contextHandle)
 {
-    if (callback == NULL)
+    if (callback != NULL)
     {
-        return;
+        i2c1Obj.callback = callback;
+        i2c1Obj.context = contextHandle;
     }
-
-    i2c1Obj.callback = callback;
-    i2c1Obj.context = contextHandle;
 }
 
 bool I2C1_IsBusy(void)
 {
-    if(I2C1STAT & _I2C1STAT_S_MASK)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return ((I2C1STAT & _I2C1STAT_S_MASK) != 0U);
 }
 
 uint8_t I2C1_ReadByte(void)
 {
-    return I2C1RCV;
+    return (uint8_t)I2C1RCV;
 }
 
 void I2C1_WriteByte(uint8_t wrByte)
 {
-    if (!(I2C1STAT & _I2C1STAT_TBF_MASK))
+    if ((I2C1STAT & _I2C1STAT_TBF_MASK) == 0U)
     {
         I2C1TRN = wrByte;
         i2c1Obj.lastByteWritten = wrByte;
@@ -223,12 +219,12 @@ void I2C1_WriteByte(uint8_t wrByte)
 
 I2C_SLAVE_TRANSFER_DIR I2C1_TransferDirGet(void)
 {
-    return (I2C1STAT & _I2C1STAT_R_W_MASK) ? I2C_SLAVE_TRANSFER_DIR_READ : I2C_SLAVE_TRANSFER_DIR_WRITE;
+    return ((I2C1STAT & _I2C1STAT_R_W_MASK) != 0U) ? I2C_SLAVE_TRANSFER_DIR_READ : I2C_SLAVE_TRANSFER_DIR_WRITE;
 }
 
 I2C_SLAVE_ACK_STATUS I2C1_LastByteAckStatusGet(void)
 {
-    return (I2C1STAT & _I2C1STAT_ACKSTAT_MASK) ? I2C_SLAVE_ACK_STATUS_RECEIVED_NAK : I2C_SLAVE_ACK_STATUS_RECEIVED_ACK;
+    return ((I2C1STAT & _I2C1STAT_ACKSTAT_MASK) != 0U) ? I2C_SLAVE_ACK_STATUS_RECEIVED_NAK : I2C_SLAVE_ACK_STATUS_RECEIVED_ACK;
 }
 
 I2C_SLAVE_ERROR I2C1_ErrorGet(void)
@@ -253,23 +249,33 @@ static void I2C1_BUS_InterruptHandler(void)
 
     if (i2c1Obj.callback != NULL)
     {
-        i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_ERROR, i2c1Obj.context);
+        uintptr_t context = i2c1Obj.context;
+
+        (void) i2c1Obj.callback(I2C_SLAVE_TRANSFER_EVENT_ERROR, context);
     }
 }
 
-static void I2C1_SLAVE_InterruptHandler(void)
+static void __attribute__((used)) I2C1_SLAVE_InterruptHandler(void)
 {
     I2C1_TransferSM();
 }
 
-void I2C_1_InterruptHandler(void)
+void __attribute__((used)) I2C_1_InterruptHandler(void)
 {
-    if ((IFS1 & _IFS1_I2C1BIF_MASK) && (IEC1 & _IEC1_I2C1BIE_MASK))
+    uint32_t iec_bus_reg_read = IEC1;
+    uint32_t iec_slave_reg_read = IEC1;
+    if (((IFS1 & _IFS1_I2C1BIF_MASK) != 0U) &&
+        ((iec_bus_reg_read & _IEC1_I2C1BIE_MASK) != 0U))
     {
         I2C1_BUS_InterruptHandler();
     }
-    else if ((IFS1 & _IFS1_I2C1SIF_MASK) && (IEC1 & _IEC1_I2C1SIE_MASK))
+    else if (((IFS1 & _IFS1_I2C1SIF_MASK) != 0U) &&
+             ((iec_slave_reg_read & _IEC1_I2C1SIE_MASK) != 0U))
     {
         I2C1_SLAVE_InterruptHandler();
+    }
+    else
+    {
+        /* Do Nothing */
     }
 }
